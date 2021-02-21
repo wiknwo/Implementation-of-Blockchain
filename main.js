9d28c987e285f83c32eb8e229ecdafbc4e71e954
wiknwo <spinningthewww@outlook.com>
2021-02-21 11:25:27 +0000

    Added test to see current state of blockchain

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
  * 
  * William Ikenna-Nwosu (wiknwo)
  */
-const SHA256 = require('crypto-js/sha-256');
+const SHA256 = require('crypto-js/sha256');
 
 class Block {
     /**
@@ -42,7 +42,38 @@ class Blockchain {
      * Constructor responsible for initializing blockchain
      */
     constructor(){
-        this.chain = []; // Array to hold chain of blocks
+        this.chain = [this.createGenesisBlock()]; // Array to hold chain of blocks
     }
 
+    /**
+     * Method creates first block in chain which must be added manually
+     */
+    createGenesisBlock(){
+        return new Block(0, '01/01/2021', "Genesis block", '0000');
+    }
+
+    /**
+     * Method returns the latest block in the chain
+     */
+    getLatestBlock(){
+        return this.chain[this.chain.length - 1];
+    }
+
+    /**
+     * 
+     * @param {Block} newBlock 
+     */
+    addBlock(newBlock){
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash(); // Every time we add a new block we need to calculate the hash
+        this.chain.push(newBlock); // In reality, you can't add a new block so easily as there are numerous checks in place
+    }
 }
+
+// Testing Blockchain
+let willieCoin = new Blockchain();
+willieCoin.addBlock(new Block(1, "10/09/2021", {amount : 4}))
+willieCoin.addBlock(new Block(2, "12/09/2021", {amount : 10}))
+willieCoin.addBlock(new Block(3, "13/09/2021", {amount : 450}))
+
+console.log(JSON.stringify(willieCoin, null, 4));
