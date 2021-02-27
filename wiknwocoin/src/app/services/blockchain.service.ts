3e106a3af2f3d7d6da21776a24a69f0ebd866c89
wiknwo <spinningthewww@outlook.com>
2021-02-27 13:58:33 +0000

    Finally downloaded dependencies and added blockchain service

--- wiknwocoin/src/app/services/blockchain.service.ts
+++ wiknwocoin/src/app/services/blockchain.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
-
+import { Blockchain } from 'wiknwocoin/src/blockchain';
+import ec from "elliptic";
 @Injectable({
   providedIn: 'root'
 })
 export class BlockchainService {
+
+  public blockchainInstance = new Blockchain();
+  public walletKeys = [];
 
   constructor() { }
 }
